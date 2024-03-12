4cd172e4ea7201d1b089c47582c4614ac7db21a3
Jakub Jagiełka <jakubjagielka2137@gmail.com>
2024-03-12 11:50:52 +0100

    3

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
-from flask import Flask, render_template, request, jsonify
-import os
+from flask import Flask, render_template, request, redirect, url_for
 
-path = os.getcwd()
 app = Flask(__name__)
-print(path)
-# Simulating a database of posts with a simple list of dictionaries
+
+# Simulate a posts database with a list
 posts = [
     {"id": 1, "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
     {"id": 2, "content": "Pellentesque vitae velit ex."},
-    {"id": 3, "content": "Mauris dapibus risus quis suscipit vulputate."}
+    {"id": 3, "content": "Mauris dapibus risus quis suscipit vulputate."},
+    {"id": 4, "content": "Mauris dadadadpibus risus quis suscipit vulputate."}
 ]
 
-
-@app.route('/')
-def index():
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        value = request.form['value']  # retrieving value from form
+        length = len(value)
     return render_template('index.html', posts=posts)
 
 
-@app.route('/add_post', methods=['POST'])
-def add_post():
-    content = request.form.get('content')
-    if content:
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        content = request.form['content']
         new_id = len(posts) + 1
         posts.append({"id": new_id, "content": content})
-        return jsonify({"id": new_id, "content": content})
-    return 'Error', 400
-
-
-@app.route('/get_posts')
-def get_posts():
-    return jsonify(posts)
-
+        return redirect(url_for('index'))
+    return render_template('index.html', posts=posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
