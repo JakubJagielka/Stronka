b67929722cc1f0599351f3bb555485e91d320712
Kacper Duda <lollel60606@gmail.com>
2024-03-13 14:46:37 +0100

    deleted unused stuff and added comments

--- main.py
+++ main.py
@@ -2,18 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for
 from optymalizacja_projekt import PostUtils
 app = Flask(__name__)
 
-# Simulate a posts database with a list
-'''posts = [
-    {"id": 1, "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.","url":"https://preview.redd.it/a-rare-hybrid-of-nord-and-imga-v0-o4ovn3il8nba1.png?width=640&crop=smart&auto=webp&s=ddc3c8006041bf1f290b21a679670cf0dfa862fb"},
-    {"id": 2, "content": "Pellentesque vitae velit ex.","url":"https://preview.redd.it/a-rare-hybrid-of-nord-and-imga-v0-o4ovn3il8nba1.png?width=640&crop=smart&auto=webp&s=ddc3c8006041bf1f290b21a679670cf0dfa862fb"},
-    {"id": 3, "content": "Mauris dapibus risus quis suscipit vulputate.","url":"https://preview.redd.it/a-rare-hybrid-of-nord-and-imga-v0-o4ovn3il8nba1.png?width=640&crop=smart&auto=webp&s=ddc3c8006041bf1f290b21a679670cf0dfa862fb"},
-    {"id": 4, "content": "Mauris dadadadpibus risus quis suscipit vulputate.","url":"https://preview.redd.it/a-rare-hybrid-of-nord-and-imga-v0-o4ovn3il8nba1.png?width=640&crop=smart&auto=webp&s=ddc3c8006041bf1f290b21a679670cf0dfa862fb"}
-]'''
+
+# start page with first 4 posts
 posts = PostUtils.getPosts().filterPosts(4, [0, 2147483646])
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    # jesli n to
     if request.method == 'POST':
         # tu biore wartosc z wejscia liczby postow
         try:
@@ -30,6 +24,7 @@ def submit():
             upperLimit = int(request.form['Upper_Limit'])
         except ValueError:
             upperLimit = 2147483646
+
         if upperLimit <= lowerLimit:
             upperLimit = lowerLimit +1
 
--- optymalizacja_projekt.py
+++ optymalizacja_projekt.py
@@ -1,29 +1,12 @@
 import requests
 
 
-'''
-CONTENT
-/posts 	    100 posts
-/comments 	500 comments
-/albums 	100 albums
-/photos 	5000 photos
-/todos 	    200 todos
-/users 	    10 users'''
-
-'''response = requests.get('https://jsonplaceholder.typicode.com/posts')
-print(response.json())'''
-# ile postow -> n postow i n zdjec
-
 class PostUtils:
     class PostList:
         def __init__(self, posts: list[dict[str, any]]) -> None:
             self.posts = posts
         
-        '''{'userId': 1,
-            'id': 1,
-            'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-            'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-            'url': 'https://via.placeholder.com/600/92c952'}'''
+        # {'userId': int, 'id': int, 'title': str, 'body': str, 'url': str}
         def filterPosts(self, amount: int,  filter: list[int]) -> list[dict[str, any]]:
             self.filteredPosts = []
             for i in self.posts:
@@ -41,8 +24,8 @@ class PostUtils:
     
 
 def main():
-    x = PostUtils.getPosts(4)
-    print(x[0])
+    x = PostUtils.getPosts()
+    print(x.posts[0])
 
 if __name__ == '__main__':
     main()
