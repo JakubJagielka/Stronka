205a47902692ebbd11229d0cb52947ef87225645
Kacper Duda <lollel60606@gmail.com>
2024-04-15 22:09:02 +0200

    usunalem niepottrzebne debug printy z klasy z utilsami

--- optymalizacja_projekt.py
+++ optymalizacja_projekt.py
@@ -13,8 +13,6 @@ class PostUtils:
                 if len(i['title']) in range(filter[0], filter[1]):
                     self.filteredPosts.append(i)
             self.filteredPosts = self.filteredPosts[:amount]
-            '''print('\tHERE ---------------------')
-            print(self.filteredPosts)'''
             return self.filteredPosts
 
     @staticmethod
@@ -30,12 +28,9 @@ class CommentUtils:
         i, maxI = 0, len(comments)
         validComments = []
         while i < maxI:
-            # print(i)
             if comments[i]['postId'] > postId:
                 break
             elif comments[i]['postId'] == postId:
-                # print(1)
                 validComments.append(comments[i])
             i += 1
-        # print(validComments)
         return validComments
