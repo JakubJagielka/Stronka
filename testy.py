ea86c98f3355ba3ee7defbcabb819b4866490790
Kacper Duda <lollel60606@gmail.com>
2024-04-15 16:14:54 +0200

    usunalem bloat z testu filtra

--- testy.py
+++ testy.py
@@ -38,27 +38,27 @@ class PostUtilsTest(unittest.TestCase):
         self.assertIsInstance(posts, PostUtils.PostList)
         self.assertEqual(len(posts.posts), 1)
     
-    @patch('testy.PostUtils.getPosts')
-    def test_filterPosts(self, mock_getPosts):
-        # Przygotowanie zmockowanych danych
+    def test_filterPosts(self):
+        # mockowy zbior postow, atrybuty ograniczylem do tylko tych istotnych dla filtra i testow
         mock_posts = [
             {"id": 1, "user_id": 11, 'title': '1'},
             {"id": 2, "user_id": 12, 'title': '12'},
             {"id": 3, "user_id": 11, 'title': '123'},
-            {"id": 4, "user_id": 13, 'title': '1234'},
-            {"id": 5, "user_id": 15, 'title': '12345'},
+            {"id": 4, "user_id": 13, 'title': '12345'},
+            {"id": 5, "user_id": 15, 'title': '1234'},
         ]
-        mock_getPosts.return_value = mock_posts
 
         # Wywołanie metody filterPosts
         post_list = PostUtils.PostList(mock_posts)
         filtered_posts = post_list.filterPosts(5, [2, 5])
 
-        # Sprawdzenie czy zwrócono oczekiwane wyniki
+        # sprawdzam czy sa tylko 3 wyniki bo tyle przepuszcza powyzszy filtr
         self.assertEqual(len(filtered_posts), 3)
-        self.assertEqual(filtered_posts[0]["user_id"], 12)
-        self.assertEqual(filtered_posts[1]["user_id"], 11)
-        self.assertEqual(filtered_posts[2]["user_id"], 13)
+        
+        # sprawdzam po kolei id postow zeby zobaczyc czy filtr przepuscil te spelniajace jego warunki
+        self.assertEqual(filtered_posts[0]["id"], 2)
+        self.assertEqual(filtered_posts[1]["id"], 3)
+        self.assertEqual(filtered_posts[2]["id"], 5)
 
 
 class CommentUtilsTest(unittest.TestCase):
