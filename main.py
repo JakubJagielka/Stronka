d39007b6e188d517f28ee367e3f1d1726c5909c8
Kacper Duda <lollel60606@gmail.com>
2024-04-23 12:44:00 +0200

    dzialajacy error msg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from optymalizacja_projekt import PostUtils, CommentUtils
+from optymalizacja_projekt import PostUtils, CommentUtils, ErrorUtils
 app = Flask(__name__)
 
 
@@ -8,30 +8,45 @@ posts = PostUtils.getPosts().filterPosts(100, [0, 2147483646])
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    errorCode = 0
     if request.method == 'POST':
         # tu prubuje wziąść wartosc z wejscia liczby postow
+        amountOfPosts = request.form['value']
+        if amountOfPosts == '':
+            amountOfPosts = 100
         try:
-            amountOfPosts = int(request.form['value'])  # zdobycie wartosci z formularza
+            amountOfPosts = int(amountOfPosts)  # zdobycie wartosci z formularza
         except ValueError:
             amountOfPosts = 100 #bazowa wartosc
+            errorCode |= 4
+
 
         # tu zbieram wartosci z 2 pol dolnego i gornego limitu dlugosci postu
-        try:
-            lowerLimit = int(request.form['Lower_Limit'])
-        except:
+        lowerLimit = request.form['Lower_Limit']
+        if lowerLimit == '':
             lowerLimit = 0
         try:
-            upperLimit = int(request.form['Upper_Limit'])
+            lowerLimit = int(lowerLimit)
+        except:
+            lowerLimit = 0
+            errorCode |= 2
+        
+        upperLimit = request.form['Upper_Limit']
+        if upperLimit == '':
+            upperLimit = 2147483646
+        try:
+            upperLimit = int(upperLimit)
         except ValueError:
             upperLimit = 2147483646
+            errorCode |= 1
 
         if upperLimit <= lowerLimit:
             upperLimit = lowerLimit +1
 
         global posts
         posts = PostUtils.getPosts().filterPosts(amountOfPosts, [lowerLimit, upperLimit])
-
-        return render_template('index.html', posts=posts)
+        errorCode = ErrorUtils.translateErrorToString(errorCode)
+        return render_template('index.html', posts=posts, errorCode = errorCode)
 
 
 @app.route('/', methods=['GET', 'POST'])
