68820c0eccec1aee834c65c71afe45d40a0ed67c
Jakub Jagiełka <jakubjagielka2137@gmail.com>
2024-05-28 11:50:06 +0200

    Nowe asynchrniczne testy

--- testy.py
+++ testy.py
@@ -1,54 +1,62 @@
-import unittest
-from locust import task, between, HttpUser
+import pytest
+import httpx
+from multiprocessing import Process
 from main import app
 from optymalizacja_projekt import PostUtils, CommentUtils
-import requests
-import cProfile
-from memory_profiler import memory_usage
 
-app = app.test_client()
-app.testing = True
+# Helper function to run the Flask app
+
+def run_app():
+
+    app.run(port=5000)
 
 
-class FlaskAppTests(unittest.TestCase):
+@pytest.fixture(scope="module", autouse=True)
+def start_flask_app():
+    proc = Process(target=run_app)
+    proc.start()
+    yield proc.terminate()
+    proc.join()
+
+class TestFlaskApp:
     """
     Testowanie porstych endpointow aplikacji, strony glownej, strony z formularzem, strony z komentarzami
     """
-    def setUp(self):
-        self.app = app
+    @pytest.mark.asyncio
+    async def test_index_page(self):
+        async with httpx.AsyncClient(base_url="http://127.0.0.1:5000") as client:
+            response = await client.get('/')
+            assert response.status_code == 200
 
-    def test_index_page(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
+    @pytest.mark.asyncio
+    async def test_post_page(self):
+        async with httpx.AsyncClient(base_url="http://127.0.0.1:5000") as client:
+            response = await client.get('/post.html?id=1')
+            assert response.status_code == 200
 
-    def test_post_page(self):
-        response = self.app.get('/post.html?id=1')
-        self.assertEqual(response.status_code, 200)
 
-    @staticmethod
-    def test_send_request(self):
+    @pytest.mark.asyncio
+    async def test_send_request(self):
         url = 'http://127.0.0.1:5000/submit'
         some_data = {
             'value': '5',
             'Lower_Limit': '10',
             'Upper_Limit': '100'
         }
+        async with httpx.AsyncClient() as client:
+            response = await client.post(url, data=some_data)
+            assert response.status_code == 200
 
-        response = requests.post(url, data=some_data)  # 'data=' sends form-encoded data
-        assert response.status_code == 200
-
-
-
-class PostUtilsTest(unittest.TestCase):
+class TestPostUtils:
     """
     Testowanie funkcji getPosts oraz filterPosts
     """
-    def test_get_posts(self):
-        pl = PostUtils.getPosts()
-        
+    @pytest.mark.asyncio
+    async def test_get_posts(self):
+        pl = await PostUtils.getPosts()
         # sprawdzam czy obiekt jest typu PostList i czy lista postow jest wieksza od 0
-        self.assertIsInstance(pl, PostUtils.PostList)
-        self.assertGreater(len(pl.posts), 0)
+        assert isinstance(pl, PostUtils.PostList)
+        assert len(pl.posts) > 0
 
 
     def test_filterPosts(self):
@@ -67,56 +75,39 @@ class PostUtilsTest(unittest.TestCase):
         filtered_posts = post_list.filterPosts(5, [2, 5])
 
         # sprawdzam czy sa tylko 3 wyniki bo tyle przepuszcza powyzszy filtr
-        self.assertEqual(len(filtered_posts), 3)
-        
+        assert len(filtered_posts) == 3
+
         # sprawdzam po kolei id postow zeby zobaczyc czy filtr przepuscil te spelniajace jego warunki
-        self.assertEqual(filtered_posts[0]["id"], 2)
-        self.assertEqual(filtered_posts[1]["id"], 3)
-        self.assertEqual(filtered_posts[2]["id"], 5)
+        assert filtered_posts[0]["id"] == 2
+        assert filtered_posts[1]["id"] == 3
+        assert filtered_posts[2]["id"] == 5
 
 
-class CommentUtilsTest(unittest.TestCase):
+class TestCommentUtils:
     """
     Testowanie funkcji getComments
     """
-    def test_get_comments(self):
-        comments = CommentUtils.getComments(1)
+    @pytest.mark.asyncio
+    async def test_get_comments(self):
+        comments = await CommentUtils.getComments(1)
         # sprawdzam czy dostalem dobry typ danych wysylajac request o komentarze
-        self.assertIsInstance(comments, list)
+        assert isinstance(comments, list)
 
 
-class Test_PerformanceTest(unittest.TestCase):
+class TestPerformance:
     """
     Testowanie wydajnosci aplikacji
     """
-
-    wait_time = between(1,5)
-
-    def setUp(self):
-        self.client = app
-
-    @task
-    def test_home_page(self):
-        self.client.get("/")
-
-    @task(3)
-    def test_comments_page(self):
-        self.client.get("/post.html?id=1")
-
-    @task
-    def test_all_comment_pages(self):
-        for i in range(1, 100):
-            self.client.get(f"/post.html?id={i}")
-
-    @task(2)
-    def test_submit_page(self):
-        data = {'value': '5', 'Lower_Limit': '10', 'Upper_Limit': '100'}
-        self.client.post('/submit', json=data)
-        self.client.get('/')
-        data = {'value': '200', 'Lower_Limit': '1', 'Upper_Limit': '100'}
-        self.client.post('/submit', json=data)
-        self.client.get('/')
-
-
-
-
+    @pytest.mark.asyncio
+    async def test_performance(self):
+        async with httpx.AsyncClient(base_url="http://127.0.0.1:5000") as client:
+            await client.get("/")
+            await client.get("/post.html?id=1")
+            for i in range(1, 100):
+                await client.get(f"/post.html?id={i}")
+            data = {'value': '5', 'Lower_Limit': '10', 'Upper_Limit': '100'}
+            await client.post('/submit', json=data)
+            await client.get('/')
+            data = {'value': '200', 'Lower_Limit': '1', 'Upper_Limit': '100'}
+            await client.post('/submit', json=data)
+            await client.get('/')
